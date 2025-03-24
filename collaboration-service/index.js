c91e52d0b6b7d64e9e0cb49fd5cb6a01fcd597cb
atharvadpatil <atharva.dp@somaiya.edu>
2025-03-24 12:34:57 +0530

    colab-service: express app created

---
+++ collaboration-service/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+//Initialize app
+const PORT = 3002;
+const app = express();
+
+//Middleware
+app.use(cors());
+app.use(express.json());
+
+
+
+//start the server
+app.listen(PORT, () => {
+    console.log(`Authentication Service is running on http://localhost:${PORT}`);
+});
