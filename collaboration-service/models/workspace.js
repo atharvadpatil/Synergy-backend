f64dbf67f36c849f69ce2d00148337752db56332
atharvadpatil <atharva.dp@somaiya.edu>
2025-03-27 12:51:31 +0530

    colab-service: workspace initial schema

---
+++ collaboration-service/models/workspace.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const workspaceSchema = mongoose.Schema({
+    name: { type: String, required: true, trim: true },
+    ownerId: { type: String, required: true },
+    ownerEmail: { type: String, required: true },
+    uniqueLink: { type: String, required: true, unique: true },
+    collaborators: [{
+        userId: { type: String, required: true },
+        userEmail: { type: String, required: true },
+        userAvatar: { type: String },
+        role: { type: String, enum: ['Editor', 'Viewer'], default: 'Editor' },
+    }],
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now }
+})
+
+const Workspace = mongoose.model("Workspace", workspaceSchema);
+
+module.exports = Workspace;
