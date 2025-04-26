050dc99f69b136ec4cd9c066446b721490ac45c5
atharvadpatil <atharva.dp@somaiya.edu>
2025-04-26 12:25:09 +0530

    codeExec-service: validateToken middleware

--- code-exec-service/index.js
+++ code-exec-service/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require("cors");
 const dotenv = require("dotenv");
 dotenv.config();
+const validateToken = require('./middlewares/validateToken');
 
 
 //Initialize app
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
+app.use(validateToken);
 
 
 app.listen(PORT, ()=>{
