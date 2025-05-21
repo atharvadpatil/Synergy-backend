b8c19dc7a1b108a1755acb328be4d77118402657
atharvadpatil <atharva.dp@somaiya.edu>
2025-05-21 11:46:10 +0530

    colab-service: yjs + websocket server

--- collaboration-service/index.js
+++ collaboration-service/index.js
@@ -6,12 +6,17 @@ const validateToken = require("./middlewares/validateToken");
 const colabRoutes = require('./routes/colab');
 dotenv.config();
 
+const http = require('http');
+const { WebSocket } = require('ws');
+const { setupWSConnection } = require('@y/websocket-server/utils');
+
 // Connect to MongoDB
 connectDB();
 
 //Initialize app
 const PORT = process.env.COLAB_SERVICE_PORT || 3002;
 const app = express();
+const server = http.createServer(app);
 
 //Middleware
 app.use(cors());
@@ -22,7 +27,14 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/colab/workspace', colabRoutes);
 
 
+// websocket server with yjs
+const wss = new WebSocket.Server({server});
+wss.on('connection', (conn, req)=>{
+    setupWSConnection(conn, req, { docName: req.url.slice(11) })
+})
+
+
 //start the server
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Collaboration Service is running on http://localhost:${PORT}`);
 });
